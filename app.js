ab6121cbbc6bd63af131ca4b5184f3406e89f546
Bridget Asser <bridgetasser@gmail.com>
2020-04-21 23:57:23 -0400

    delete book added to UI prototype

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ UI.prototype.showAlert = function (message, className) {
     }, 3000);
 };
 
+UI.prototype.deleteBook = function (target) {
+    if (target.className === "delete") {
+        target.parentElement.parentElement.remove();
+    }
+};
+
 UI.prototype.clearFields = function () {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
@@ -62,6 +68,16 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 
         ui.clearFields();
     }
+
+    e.preventDefault();
+});
+
+document.getElementById("book-list").addEventListener("click", function (e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    ui.showAlert("Book Removed", "success");
 
     e.preventDefault();
 });
